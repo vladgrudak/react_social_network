f4f03fb7692fefc52c20256a250266debea7fc96
vladgrudak <ralkavladislav@gmail.com>
2023-09-29 18:51:04 +0300

    First commmit. Lesson 12. Was created components Header, Navbar, Profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://e7.pngegg.com/pngimages/41/701/png-clipart-logo-graphic-design-graphy-grapher-logo-graphy-angle-text.png"
+                width="50px" alt="Logo"/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div className="long-image">
+                <img src="https://a.l3n.co/i/qi0Br.jpg" alt="longimage"/>
+            </div>
+            <div className="ava">
+                <img src="https://wallpapers.com/images/hd/square-pictures-3000-x-3000-fiqulodtb0q3p20y.jpg"
+                     alt="squareimage"/>
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post2
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
