072bea16083ad74875b880057d4ea5b17c314a62
vladgrudak <ralkavladislav@gmail.com>
2023-09-29 20:26:07 +0300

    Lesson 14. Were added css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from'./Header.module.css';
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={s.header}>
             <img
                 src="https://e7.pngegg.com/pngimages/41/701/png-clipart-logo-graphic-design-graphy-grapher-logo-graphy-angle-text.png"
                 width="50px" alt="Logo"/>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className="content">
-            <div className="long-image">
+        <div className={s.content}>
+            <div className={s.longImage}>
                 <img src="https://a.l3n.co/i/qi0Br.jpg" alt="longimage"/>
             </div>
-            <div className="ava">
+            <div className={s.ava}>
                 <img src="https://wallpapers.com/images/hd/square-pictures-3000-x-3000-fiqulodtb0q3p20y.jpg"
                      alt="squareimage"/>
             </div>
@@ -15,11 +16,11 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>
+                <div className={s.post}>
+                    <div className={s.item}>
                         Post1
                     </div>
-                    <div>
+                    <div className={s.item}>
                         Post2
                     </div>
                 </div>
